b09385a72d0a36dc352ee38a4aec80123447a7a6
saravatss <149174589+saravatss@users.noreply.github.com>
2024-06-27 09:16:23 +0300

    final

--- src/db/users-books.js
+++ src/db/users-books.js
@@ -3,6 +3,16 @@ import { Router } from '../../core/index.js';
 
 const usersRouter = new Router();
 
+usersRouter.get('/users', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    const DBUsersBooks = new DBJson('./src/db/users.json'); 
+
+    response.end(JSON.stringify(DBUsersBooks.getAll()));
+});
+
 usersRouter.post('/users/books', (request, response) => {
     response.writeHead(200, {
         'Content-type': 'application/json'
@@ -16,12 +26,12 @@ usersRouter.post('/users/books', (request, response) => {
         })
         .on('end', () => {
             const newUser = JSON.parse(data);
-            const DBUsersBooks = JSON.parse(readFileSync('./src/db/users-books.json', 'utf-8'));
+            const DBUsersBooks = JSON.parse(readFileSync('./src/db/users-books.json'));
 
             const hasUser = DBUsersBooks.find(user => user.userId === parseInt(newUser.userId));
 
             if (!hasUser) {
-                DBUsersBooks.push(newUser);
+                DBUsersBooks.add(newUser);
 
                 writeFileSync('./src/db/users-books.json', JSON.stringify(DBUsersBooks));
 
@@ -33,3 +43,124 @@ usersRouter.post('/users/books', (request, response) => {
             }
         });
 });
+
+
+migraineRouter.post('/migraine', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const newMigraine = JSON.parse(data);
+
+            const DBMigraine = new DBJson('./src/db/migraines.json');
+
+            const success = DBMigraine.add(newMigraine);
+
+            if (success) {
+                response.end(JSON.stringify(DBMigraine.getAll()));
+            } else {
+                response.end('Не удалось добавить запись');
+            }
+        });
+});
+
+
+/* migraineRouter.get('/migraine', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    const DBMigraine = new DBJson('./src/db/migraines.json'); 
+
+    response.end(JSON.stringify(DBMigraine.getAll()));
+});
+
+migraineRouter.post('/migraine', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const newMigraine = JSON.parse(data);
+
+            const DBMigraine = new DBJson('./src/db/migraines.json');
+
+            const success = DBMigraine.add(newMigraine);
+
+            if (success) {
+                response.end(JSON.stringify(DBMigraine.getAll()));
+            } else {
+                response.end('Не удалось добавить запись');
+            }
+        });
+});
+
+//обновление списка
+migraineRouter.put('/migraine', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const url = new URL('http://127.0.0.1:8080' + request.url);
+            const queryId = parseInt(url.searchParams.get('id'));
+
+            const payload = JSON.parse(data);
+
+            const DBMigraine = new DBJson('./src/db/migraines.json');
+
+            const success = DBMigraine.updateById(queryId, payload);
+
+            if (success) {
+                response.end(JSON.stringify(DBMigraine.getAll()));
+            } else {
+                response.end('Не удалось обновить запись');
+            }
+        });
+});
+
+migraineRouter.delete('/migraine', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const payload = JSON.parse(data);
+
+            const DBMigraine = new DBJson('./src/db/migraines.json');
+
+            const success = DBMigraine.deleteById(payload.id);
+
+            if (success) {
+                response.end(JSON.stringify(DBMigraine.getAll()));
+            } else {
+                response.end('Не удалось удалить запись');
+            }
+        });
+});
+*/
--- src/routers/books.js
+++ src/routers/books.js
@@ -1,5 +1,4 @@
-import { readFileSync } from 'node:fs';
-import { Router } from '../../core/index.js';
+import { Router, DBJson } from '../../core/index.js';
 
 const booksRouter = new Router();
 
@@ -8,9 +7,90 @@ booksRouter.get('/books', (request, response) => {
         'Content-type': 'application/json'
     });
 
-    const DBBooks = JSON.parse(readFileSync('./src/db/books.json', 'utf-8'));
+    const DBBooks = new DBJson('./src/db/books.json'); 
 
-    response.end(JSON.stringify(DBBooks));
+    response.end(JSON.stringify(DBBooks.getAll()));
+});
+
+booksRouter.post('/books', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const newBook = JSON.parse(data);
+
+            const DBBooks = new DBJson('./src/db/books.json');
+
+            const success = DBBooks.add(newBook);
+
+            if (success) {
+                response.end(JSON.stringify(DBBooks.getAll()));
+            } else {
+                response.end('Не удалось добавить книгу');
+            }
+        });
+});
+
+booksRouter.put('/books', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const url = new URL('http://127.0.0.1:8080' + request.url);
+            const queryId = parseInt(url.searchParams.get('id'));
+
+            const payload = JSON.parse(data);
+
+            const DBBooks = new DBJson('./src/db/books.json');
+
+            const success = DBBooks.updateById(queryId, payload);
+
+            if (success) {
+                response.end(JSON.stringify(DBBooks.getAll()));
+            } else {
+                response.end('Не удалось обновить данные книги');
+            }
+        });
+});
+
+booksRouter.delete('/books', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const payload = JSON.parse(data);
+
+            const DBBooks = new DBJson('./src/db/books.json');
+
+            const success = DBBooks.deleteById(payload.id);
+
+            if (success) {
+                response.end(JSON.stringify(DBBooks.getAll()));
+            } else {
+                response.end('Не удалось удалить книгу');
+            }
+        });
 });
 
 export {
